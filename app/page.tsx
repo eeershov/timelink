eadc0208b8a7acd2f430e3e3b35a52d4b1f81a50
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2024-01-31 16:14:22 +0300

    refactor: css

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ export default function Home() {
   ]
 
   return (
-    <main className='flex min-h-screen w-full flex-col items-center justify-center gap-3 bg-[#AEDADA] px-2 xl:px-12 2xl:gap-5 2xl:px-24'>
+    <main className='flex min-h-screen w-full flex-col items-center justify-center gap-3 bg-[#AEDADA] px-2 py-2 md:py-0 xl:px-12 2xl:gap-5 2xl:px-24'>
       <div className='flex h-auto min-w-full items-center justify-center rounded-2xl bg-white 2xl:rounded-3xl'>
         <div className='my-11 w-min md:my-28 2xl:my-32'>
           <h1
-            className={`${urbanist.className} text-center text-6xl font-normal text-[#5AB9B9] sm:text-7xl md:text-8xl 2xl:text-9xl`}
+            className={`${urbanist.className} text-center text-5xl font-normal text-[#5AB9B9] sm:text-7xl md:text-8xl 2xl:text-9xl`}
           >
             LinkTimeZone
           </h1>
